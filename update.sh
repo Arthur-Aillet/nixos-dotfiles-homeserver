5200812848239ec400018f6cd8cb51c0cc0de8ee
Arthur-Aillet <arthur.aillet@epitech.eu>
2025-09-07 18:57:58 +0200

    fix: removed nixos switch log from repository, updated update sh

--- update.sh
+++ update.sh
@@ -3,11 +3,13 @@
 
 pushd /home/user/.dotfiles/
 
-if git diff --quiet '*.nix'; then
+if git diff --quiet; then
     echo "No changes detected, exiting."
     popd
     exit 0
 fi
+
+git add .
 
 alejandra . &>/dev/null \
   || ( alejandra . ; echo "formatting failed!" && exit 1)
@@ -16,12 +18,10 @@ git diff -U0 '*.nix'
 
 echo "NixOS Rebuilding..."
 
-git add .
-
 # Rebuild, output simplified errors, log trackebacks
 sudo nixos-rebuild switch  --flake /home/user/.dotfiles &>nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1)
 
-# Commit all changes witih the generation metadata
+# Commit all changes
 git commit
 
 # Back to where you were
